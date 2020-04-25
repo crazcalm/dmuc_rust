4147bc40c5af27ec8d56425bea00cc818ab98691
Marcus Willock <crazcalm@gmail.com>
2020-04-25 16:10:19 -0400

    MVP of dmuc in rust
    ticket: https://tree.taiga.io/project/crazcalm-youtube-rust/task/3

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 extern crate clap;
 use clap::App;
 
+use std::ffi::OsStr;
+use std::path::PathBuf;
 use std::{fs, io, path};
 
 fn start_with_check(item: &str, start_with: &str) -> bool {
@@ -72,58 +74,138 @@ fn ls_attempt(path: &path::Path) -> io::Result<Vec<path::PathBuf>> {
     // The order in which `read_dir` returns entries is not guaranteed. If reproducible
     // ordering is required the entries should be explicitly sorted.
 
+    // Sorts the entries by their path
     entries.sort();
 
+    /*
     // The entries have now been sorted by their path.
-
     for file in &entries {
         if let Some(cmd) = file.file_name() {
             println!("file: {:?}", cmd);
         }
     }
+    */
 
     Ok(entries)
 }
 
+fn print_to_screen(paths: Vec<PathBuf>, header: &str) {
+    println!("{}:\n", header);
+
+    for path in paths {
+        match path.file_name() {
+            Some(file_name) => match file_name.to_str() {
+                Some(name) => {
+                    println!("{}", name);
+                }
+                None => {
+                    eprintln!("unable to convert file name into string: {:?}", file_name);
+                }
+            },
+            None => {
+                eprintln!("Unable to convert file path to string: {:?}", path);
+            }
+        }
+    }
+}
+
+pub enum Filter<'a> {
+    Startswith(&'a str),
+    Endswith(&'a str),
+    Includes(&'a str),
+    None,
+}
+
+fn dmuc(path: &path::Path, filter: &Filter) {
+    let mut results = ls_attempt(path).unwrap();
+
+    let results = match filter {
+        Filter::Startswith(string) => start_with_list(results, string),
+        Filter::Includes(string) => includes_list(results, string),
+        Filter::Endswith(string) => ends_with_list(results, string),
+        Filter::None => results,
+    };
+
+    print_to_screen(results, path.to_str().unwrap());
+}
+
+fn dmuc_with_list(paths: Vec<&path::Path>, filter: &Filter) {
+    for path in paths {
+        dmuc(path, &filter);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    //Defining my directory variables
+    let usr_bin = "/usr/bin";
+    let local_bin = "/usr/local/bin";
+    let both_dirs = vec![usr_bin.as_ref(), local_bin.as_ref()];
+
+
+    // CMD flags
+    let local = "local";
+    let all = "all";
+    let startswith = "startswith";
+    let includes = "includes";
+    let endswith = "endswith";
 
     // The YAML file is found relative to the current file, similar to how modules are found
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    println!("matches: {:?}", matches);
+    // If there are not flags, do the default
+    if matches.args.is_empty() {
+        let path = usr_bin.as_ref();
 
-    if let Some(v) = matches.value_of("includes") {
-        println!("includes: {:?}", v);
+        dmuc(path, &Filter::None);
+
+    } else if matches.is_present(local) && matches.is_present(startswith) {
+        let path = local_bin.as_ref();
+        let filter = Filter::Startswith(matches.value_of(startswith).unwrap());
+
+        dmuc(path, &filter);
+    } else if matches.is_present(local) && matches.is_present(includes) {
+        let path = local_bin.as_ref();
+        let filter = Filter::Includes(matches.value_of(includes).unwrap());
+
+        dmuc(path, &filter);
+    } else if matches.is_present(local) && matches.is_present(endswith) {
+        let path = local_bin.as_ref();
+        let filter = Filter::Endswith(matches.value_of(endswith).unwrap());
+
+        dmuc(path, &filter);
+
+    } else if matches.is_present(local) {
+        let path = local_bin.as_ref();
+        dmuc(path, &Filter::None);
+
+    } else if matches.is_present(all) && matches.is_present(startswith) {
+        let filter = Filter::Startswith(matches.value_of(startswith).unwrap());
+        dmuc_with_list(both_dirs, &filter);
+
+    } else if matches.is_present(all) && matches.is_present(includes) {
+        let filter = Filter::Includes(matches.value_of(includes).unwrap());
+        dmuc_with_list(both_dirs, &filter);
+
+    } else if matches.is_present(all) && matches.is_present(endswith) {
+        let filter = Filter::Endswith(matches.value_of(endswith).unwrap());
+        dmuc_with_list(both_dirs, &filter);
+
+    } else if matches.is_present(all) {
+        dmuc_with_list(both_dirs, &Filter::None)
+    }else if matches.is_present(includes) {
+        let path = usr_bin.as_ref();
+        let filter = Filter::Includes(matches.value_of(includes).unwrap());
+        dmuc(path, &filter);
+    } else if matches.is_present(startswith) {
+        let path = usr_bin.as_ref();
+        let filter = Filter::Startswith(matches.value_of(startswith).unwrap());
+
+        dmuc(path, &filter);
+    } else if matches.is_present(endswith) {
+        let path = usr_bin.as_ref();
+        let filter = Filter::Endswith(matches.value_of(endswith).unwrap());
+
+        dmuc(path, &filter);
     }
-
-    let all = matches.is_present("all");
-
-    if all {
-        println!("all is present");
-    } else {
-        println!("all is not present");
-    }
-
-    let usr_bin = "/usr/bin";
-    let local_bin = "/usr/local/bin";
-
-    ls_attempt(local_bin.as_ref());
-
-    let mut testing = ls_attempt(usr_bin.as_ref()).unwrap();
-
-    println!("pre-testing: {:?}", testing);
-
-    let mut testing2 = start_with_list(testing.clone(), "zip");
-
-    println!("starts_with: {:?}", testing2);
-
-    let mut testing2 = ends_with_list(testing.clone(), "zip");
-
-    println!("ends_with: {:?}", testing2);
-
-    let mut testing2 = includes_list(testing.clone(), "zip");
-
-    println!("includes: {:?}", testing2);
 }
